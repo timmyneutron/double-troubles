86b9940ca375c88a90c50f2ebb21628b60ac1fac
Tim Abrahamsen <timmy_neutron@mac.com>
2026-04-02 10:29:54 -0400

    fix workshop time

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
       <h2>Workshop Announcement</h2>
       <p className="description">
         We're hosting an ambidancetrous Lindy Hop workshop on <strong>Wednesday, April 8th </strong>
-        from 7:30 PM to 9:00 PM. For more details check out the FB event page <a href="https://www.facebook.com/events/1498235402020134" target="_blank" rel="noopener noreferrer">here</a>. See you there!
+        from 7:30 PM to 9:30 PM. For more details check out the FB event page <a href="https://www.facebook.com/events/1498235402020134" target="_blank" rel="noopener noreferrer">here</a>. See you there!
       </p>
       <h2>Get in Touch</h2>
       <p className="description">
