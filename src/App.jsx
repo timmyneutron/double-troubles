f7964f689c793e9f1eeeeacedd97a695299db662
Tim Abrahamsen <timmy_neutron@mac.com>
2026-05-30 00:00:38 -0400

    remove workshop

--- src/App.jsx
+++ src/App.jsx
@@ -46,13 +46,6 @@ function App() {
         to come dance with us!
       </p>
 
-      <h2>Workshop Announcement</h2>
-      <p className="description">
-        We're having a workshop! Join us Wednesday, May 27th, 7:30 - 9:30 PM for an ambidancetrous Lindy Hop **performance** workshop.
-        <br/>
-        <a href="https://www.facebook.com/events/1512667526981637" target="_blank" rel="noopener noreferrer">Click here for details</a>
-      </p>
-
       <h2>Summer Term Starts June 3rd!</h2>
       <p className="description">
         We're gearing up for our summer term, which starts Wednesday, June 3rd! If you're interested in joining us, please fill out the form below and we'll get back to you with more information about the upcoming term.
